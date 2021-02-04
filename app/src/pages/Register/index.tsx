aa89bc7fbc629d887b43fc1cb879073cd5057958
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-04 20:14:09 -0300

    Ajustado tela de cadastro dos colaboradores

--- app/src/pages/Register/index.tsx
+++ app/src/pages/Register/index.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useCallback } from 'react';
-import Select, { components } from 'react-select';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import { FiArrowLeft, FiSave, FiMail, FiUser, FiAward } from 'react-icons/fi';
 import { Link, useHistory } from 'react-router-dom';
@@ -17,50 +17,59 @@ import Footer from '../../components/Footer';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import { useToast } from '../../hooks/toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+
 interface RegisterFormData {
   name: string;
   email: string;
+  suggestion: string;
 }
 
 const Register: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
   const history = useHistory();
-
-  const customStyleSelect = {
-    control: (base: any, state: any) => ({
-      ...base,
-      padding: "6px",
-      background: "#d9d9d9",
-      color: state.isFocused ? "#666360" : "#004de6",
-      borderRadius: state.isFocused ? "5px" : 10,
-      borderColor: state.isFocused ? "#cccccc" : "#d9d9d9",
-      border: "2px solid #ccc",
-      boxShadow: state.isFocused ? null : null,
-      "&:hover": {
-        borderColor: state.isFocused ? "#0055ff" : "#d9d9d9",
-      },
-    }),
-  };
-
-  const teams = [
-    { value: 'team-developers', label: 'Equipe do Desenvolvimento' },
-    { value: 'team-devops', label: 'Equipe de DevOps' },
-    { value: 'team-rh', label: 'Equipe do RH' },
-  ];
 
   const handleSubmit = useCallback(
     async (data: RegisterFormData) => {
       try {
         formRef.current?.setErrors({});
 
-        // Actions
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome é obrigatório'),
+          email: Yup.string()
+            .required('E-mail é obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
         history.push('/choose');
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado!',
+          description: 'Você já pode votar em um restaurante disponível!',
+        });
       } catch (err) {
-        // Error
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro',
+          description: 'Ocorreu um erro ao fazer cadastro, tente novamente.',
+        });
       }
     },
-    [history],
+    [addToast, history],
   );
 
   return (
@@ -70,11 +79,23 @@ const Register: React.FC = () => {
       <Content>
         <Section>
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <Input name="suggestion" icon={FiAward} placeholder="Sugestão de Restaurante" />
-            <Input name="name" icon={FiUser} placeholder="Nome do Colaborador" />
-            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="suggestion"
+              icon={FiAward}
+              placeholder="Sugestão de Restaurante"
+            />
 
-            <Select options={teams} styles={customStyleSelect} placeholder="Selecione seu Time" />
+            <Input
+              name="name"
+              icon={FiUser}
+              placeholder="Nome do Colaborador"
+            />
+
+            <Input
+              name="email"
+              icon={FiMail}
+              placeholder="E-mail"
+            />
 
             <Button type="submit">
               <FiSave /> Salvar
