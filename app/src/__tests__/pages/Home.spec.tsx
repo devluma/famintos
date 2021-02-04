c12ec80cfd910e7f6b288fda4524fc6ae787bfc9
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-04 20:18:05 -0300

    Adicionado arquivos de testes de exemplo

---
+++ app/src/__tests__/pages/Home.spec.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import SignIn from '../../pages/Home';
+
+const mockedHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: mockedHistoryPush,
+  }),
+  Link: ({ children }: { children: React.ReactNode }) => children,
+}));
+
+describe('Home Page', () => {
+  beforeEach(() => {
+    mockedHistoryPush.mockClear();
+  });
+
+  it('should not be able to sign in with invalid credentials', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'not-valid-email' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalled();
+    });
+  });
+});
