1fc0941af62c491944a76da5521d986a0500c34f
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-05 21:24:25 -0300

    Adicionado helper para gerar o hash

--- api/src/utils/generateHash.js
+++ api/src/utils/generateHash.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto-js');
 
-module.exports = function generateUniqueId(text = process.env.API_SECRET) {
+module.exports = function generateHash(text = process.env.API_SECRET) {
   const hash = crypto.SHA256(text);
 
   return hash.toString(crypto.enc.Base64);
