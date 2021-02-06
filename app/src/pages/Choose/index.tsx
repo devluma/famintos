c5c0560fda94b8980b6e53516e8aee8ef5620d01
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-06 18:40:01 -0300

    Ajustado página de votação

--- app/src/pages/Choose/index.tsx
+++ app/src/pages/Choose/index.tsx
@@ -1,5 +1,9 @@
-import React, { useCallback } from 'react';
-import { FiX, FiThumbsUp } from 'react-icons/fi';
+import React, { useRef, useCallback } from 'react';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import { FiX, FiThumbsUp, FiAward, FiSearch } from 'react-icons/fi';
 import { Link, useHistory } from 'react-router-dom';
 
 import {
@@ -12,9 +16,58 @@ import {
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Input from '../../components/Input';
+// import Button from '../../components/Button';
+
+import { useToast } from '../../hooks/toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+interface ChooseFormData {
+  suggestion: string;
+  like: boolean;
+}
 
 const Choose: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
   const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: ChooseFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          suggestion: Yup.string().required('Sugestão de restaurante é obrigatório'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        history.push('/choose');
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado!',
+          description: 'Você adicionou um novo restaurante!',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro',
+          description: 'Ocorreu um erro ao fazer o cadastro, tente novamente.',
+        });
+      }
+    },
+    [addToast, history],
+  );
 
   const handleLiked = useCallback(
     async () => {
@@ -34,69 +87,83 @@ const Choose: React.FC = () => {
 
       <Content>
         <Section>
-          <h3>Lista dos Restaurantes Disponíveis</h3>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h3>Lista dos Restaurantes Disponíveis</h3>
 
-          <ListContainer>
-            <ItemContent>
-              <strong>Restaurante do João</strong>
-              <span>&gt;&gt;</span>
-              <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
+            <div>
+              <Input
+                name="suggestion"
+                icon={FiAward}
+                placeholder="Sugestão de Restaurante"
+              />
 
-              <div>
-                <button type="button" onClick={handleLiked}>
-                  <FiThumbsUp size={20} color="#0055ff" />
-                </button>
-              </div>
-            </ItemContent>
+              <button type="button">
+                <FiSearch size={40} color="#0055ff" />
+              </button>
+            </div>
 
-            <ItemContent>
-              <strong>Restaurante do João</strong>
-              <span>&gt;&gt;</span>
-              <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
+            <ListContainer>
+              <ItemContent>
+                <strong>Restaurante do João</strong>
+                <span>&gt;&gt;</span>
+                <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
 
-              <div>
-                <button type="button" onClick={handleLiked}>
-                  <FiThumbsUp size={20} color="#0055ff" />
-                </button>
-              </div>
-            </ItemContent>
+                <div>
+                  <button type="button" onClick={handleLiked}>
+                    <FiThumbsUp size={20} color="#0055ff" />
+                  </button>
+                </div>
+              </ItemContent>
 
-            <ItemContent>
-              <strong>Restaurante do João</strong>
-              <span>&gt;&gt;</span>
-              <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
+              <ItemContent>
+                <strong>Restaurante do João</strong>
+                <span>&gt;&gt;</span>
+                <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
 
-              <div>
-                <button type="button" onClick={handleLiked}>
-                  <FiThumbsUp size={20} color="#0055ff" />
-                </button>
-              </div>
-            </ItemContent>
+                <div>
+                  <button type="button" onClick={handleLiked}>
+                    <FiThumbsUp size={20} color="#0055ff" />
+                  </button>
+                </div>
+              </ItemContent>
 
-            <ItemContent>
-              <strong>Restaurante do João</strong>
-              <span>&gt;&gt;</span>
-              <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
+              <ItemContent>
+                <strong>Restaurante do João</strong>
+                <span>&gt;&gt;</span>
+                <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
 
-              <div>
-                <button type="button" onClick={handleLiked}>
-                  <FiThumbsUp size={20} color="#0055ff" />
-                </button>
-              </div>
-            </ItemContent>
+                <div>
+                  <button type="button" onClick={handleLiked}>
+                    <FiThumbsUp size={20} color="#0055ff" />
+                  </button>
+                </div>
+              </ItemContent>
 
-            <ItemContent>
-              <strong>Restaurante do João</strong>
-              <span>&gt;&gt;</span>
-              <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
+              <ItemContent>
+                <strong>Restaurante do João</strong>
+                <span>&gt;&gt;</span>
+                <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
 
-              <div>
-                <button type="button" onClick={handleLiked}>
-                  <FiThumbsUp size={20} color="#0055ff" />
-                </button>
-              </div>
-            </ItemContent>
-          </ListContainer>
+                <div>
+                  <button type="button" onClick={handleLiked}>
+                    <FiThumbsUp size={20} color="#0055ff" />
+                  </button>
+                </div>
+              </ItemContent>
+
+              <ItemContent>
+                <strong>Restaurante do João</strong>
+                <span>&gt;&gt;</span>
+                <p><b>Este Restaurante</b> já foi escolhido <b>6</b> vezes</p>
+
+                <div>
+                  <button type="button" onClick={handleLiked}>
+                    <FiThumbsUp size={20} color="#0055ff" />
+                  </button>
+                </div>
+              </ItemContent>
+            </ListContainer>
+          </Form>
         </Section>
 
         <Link to="/">
