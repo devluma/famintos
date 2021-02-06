9777545fcca6ff9f8df2a72761469718a708a1c8
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-05 21:15:57 -0300

    Ajustado rotas da aplicação

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,15 +1,128 @@
 const express = require('express');
-// const { celebrate, Segments, Joi } = require('celebrate');
+const { celebrate, Segments, Joi } = require('celebrate');
 
-const RestaurantController = require('./controllers/RestaurantController');
+const SessionsController = require('./controllers/SessionsController');
+const UsersController = require('./controllers/UsersController');
+const RestaurantsController = require('./controllers/RestaurantsController');
 
 const routes = express.Router();
 
 // Rotas de Sessão
+routes.get('/sessions/logout', SessionsController.logout);
+routes.get('/sessions/verify', SessionsController.verify);
+routes.post(
+  '/sessions',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  SessionsController.authenticate
+);
+
+// Rotas para Usuários
+routes.get(
+  '/users',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+      limit: Joi.number(),
+    }),
+  }),
+  UsersController.index
+);
+routes.get(
+  '/users/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  UsersController.show
+);
+routes.post(
+  '/users',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  UsersController.store
+);
+routes.put(
+  '/users/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+    }),
+  }),
+  UsersController.update
+);
+routes.delete(
+  '/users/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  UsersController.destroy
+);
 
 // Rotas para Restaurantes
-routes.get('/restaurants', RestaurantController.index);
-
-// Rotas para Colaboradores
+routes.get(
+  '/restaurants',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+      limit: Joi.number(),
+    }),
+  }),
+  RestaurantsController.index
+);
+routes.get(
+  '/restaurants/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  RestaurantsController.show
+);
+routes.post(
+  '/restaurants',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+    }),
+  }),
+  RestaurantsController.store
+);
+routes.put(
+  '/restaurants/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+    }),
+  }),
+  RestaurantsController.update
+);
+routes.delete(
+  '/restaurants/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  RestaurantsController.destroy
+);
 
 module.exports = routes;
