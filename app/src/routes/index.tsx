6cbafed481cc6c2fbb2f36418a382207297f756e
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-06 18:36:28 -0300

    Ajustado rotas e redirecionamentos

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import Home from '../pages/Home';
+import SignIn from '../pages/SignIn';
 import Register from '../pages/Register';
 import Choose from '../pages/Choose';
 import Dashboard from '../pages/Dashboard';
@@ -9,9 +12,11 @@ import Dashboard from '../pages/Dashboard';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
-    <Route path="/register" exact component={Register} />
-    <Route path="/choose" component={Choose} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/register" component={Register} />
     <Route path="/dashboard" component={Dashboard} />
+
+    <Route path="/choose" component={Choose} isPrivate />
   </Switch>
 );
 
