220a798eedf0b8d233760dba9392233e34c6c4a6
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-07 20:09:40 -0300

    Adicionado dados do usuário no schedule

--- api/src/utils/generateSchedule.js
+++ api/src/utils/generateSchedule.js
@@ -1,11 +1,19 @@
-module.exports = function generateSchedule() {
+module.exports = function generateSchedule(user = null) {
   const date = new Date();
   const schedule = {
     year: date.getFullYear(),
     month: date.getMonth(),
     day: date.getDate(),
     time: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+    user: {},
   };
+
+  if (user) {
+    schedule.user = {
+      id: user.id,
+      name: user.name,
+    };
+  }
 
   return JSON.stringify(schedule);
 };
