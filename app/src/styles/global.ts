e1569d4ed6708e39f6f419047f787cfbe07b5c50
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-07 00:21:09 -0300

    Ajustado style global da aplicação

--- app/src/styles/global.ts
+++ app/src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import { shade } from 'polished';
 
 export default createGlobalStyle`
   * {
@@ -31,6 +32,36 @@ export default createGlobalStyle`
     cursor: pointer;
   }
 
+  .react-confirm-alert-button-group button {
+    background: #99bbff;
+    height: 56px;
+    border-radius: 10px;
+    border: 1px solid #0055ff;
+    padding: 0 16px;
+    color: #666360;
+    text-align: center;
+    float: left;
+    width: 100%;
+    font-weight: 500;
+    margin-top: 16px;
+    transition: background-color 0.2s;
+
+    /* flex para alinhar conteúdo*/
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      background: ${shade(0.2, '#80aaff')};
+      color: #fff;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+      margin-right: 3px;
+    }
+  }
 
   /* Extra small devices (phones, 600px and down) */
   @media only screen and (max-width: 600px) {
