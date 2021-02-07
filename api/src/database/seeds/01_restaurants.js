352fbb83002aaa800e04b15b189a856108b721a0
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-07 20:29:55 -0300

    Ajustado seeds dos restaurantes

--- api/src/database/seeds/01_restaurants.js
+++ api/src/database/seeds/01_restaurants.js
@@ -6,13 +6,10 @@ exports.seed = async (knex) => {
       // Inserts seed entries
       await knex('restaurants').insert([
         { id: 1, name: 'Restaurante do Galego' },
-        { id: 2, name: 'Restaurante Mais Você' },
-        { id: 3, name: 'Restaurante da PUC' },
-        { id: 4, name: 'Lancheria do Seu Zé' },
-        { id: 5, name: 'Lancheria Dona Maria' },
-        { id: 6, name: 'Ifood Xis Cavanhas' },
-        { id: 7, name: 'Ifood Prato Feito' },
-        { id: 8, name: 'Pizza' },
+        { id: 2, name: 'Restaurante da PUC' },
+        { id: 3, name: 'Xis Cavanhas' },
+        { id: 4, name: 'Ifood Prato Feito' },
+        { id: 5, name: 'Pizza' },
       ]);
     });
 };
