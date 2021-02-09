bc435335f696f94671b48ab30c135251199333ad
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-09 17:43:29 -0300

    Ajustando o helper dos schedules de ativação

--- api/src/utils/generateSchedule.js
+++ api/src/utils/generateSchedule.js
@@ -1,23 +1,45 @@
 module.exports = function generateSchedule(typeReturn = 'STRING') {
   try {
     const date = new Date();
-
     const workDays = [1, 2, 3, 4, 5];
     const daysOfWeek = [7, 1, 2, 3, 4, 5, 6];
-    const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const weeksName = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+    const monthsName = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
 
     const schedule = {
       year: date.getFullYear(),
-      month: date.getMonth(),
+      month: date.getMonth() + 1,
+      monthsName: monthsName[date.getMonth()],
       day: date.getDate(),
       dayWeek: daysOfWeek[date.getDay()],
-      weekDay: week[date.getDay()],
+      weekDay: weeksName[date.getDay()],
       workDay: workDays[date.getDay()],
       workDays,
       daysOfWeek,
-      week,
-      time: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+      weeksName,
       lastDays: date.setDate(date.getDate() - daysOfWeek.length),
+      time: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
     };
 
     const dataReturn = JSON.stringify(schedule);
