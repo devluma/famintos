52e1052bcaefee3b5c82c3b5524168aeef7ac84c
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-09 17:40:08 -0300

    Ajustado as migrations da aplicação

--- api/src/database/migrations/20210204210245_create_users_table.js
+++ api/src/database/migrations/20210204210245_create_users_table.js
@@ -3,7 +3,7 @@ exports.up = async (knex) => {
     table.increments('id').unique().notNullable().primary();
     table.string('name', 80).notNullable();
     table.string('email', 100).unique().notNullable();
-    table.string('password').notNullable();
+    table.string('password', 44).notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
