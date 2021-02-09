2b3ecf9aaa5468e897a9c812418cfc935aaecfaa
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-02-09 19:42:22 -0300

    Ajustanso testes da API

--- api/src/__tests__/unit/generateSchedule.spec.js
+++ api/src/__tests__/unit/generateSchedule.spec.js
@@ -5,22 +5,6 @@ describe('Generate a Schedule of Dates', () => {
     const scheduleString = generateSchedule('STRING');
     const scheduleJson = generateSchedule('JSON');
 
-    // expect(scheduleJson).toEqual({
-    //   year: 2021,
-    //   month: 2,
-    //   day: 8,
-    //   dayWeek: 1,
-    //   weekDay: expect.toString(),
-    //   workDay: 1,
-    //   workDays: [1, 2, 3, 4, 5],
-    //   daysOfWeek: [1, 2, 3, 4, 5, 6, 7],
-    //   week: [7, 1, 2, 3, 4, 5, 6],
-    //   time: expect.toString(),
-    //   lastDays: expect.toString(),
-    // });
-
-    // console.log(scheduleJson);
-
     expect(scheduleJson).toMatchObject({
       year: expect.toNumber().toLength(4),
       month: 2,
